b2b608276dc9d9d15d1e1ec40118716faf68cbc7
ADL175 <armydavidlim@gmail.com>
2017-03-07 15:54:32 -0800

    save commit

--- poop.js
+++ poop.js
@@ -1,4 +1,11 @@
 "use strict";
+var name= prompt("Hello, what's your name?");
+
+function greeting () {
+  alert("Good to meet you " + name + "!");
+  console.log('name:' + name);
+}
+greeting();
 
 confirm ("Let's play a guessing game!");
 
@@ -12,66 +19,42 @@ if (userFood.toLowerCase () === 'yes' || userFood.toLowerCase() === 'y'){
   alert('You\'re silly, type yes or no');
 }
 
-var userMovie = prompt('Guess my favorite television show!');
+var userOne = prompt('Do I like Indian food?');
+console.log('indian' + ":" + userOne);
+if (userOne.toLowerCase () === 'yes' || userOne.toLowerCase() === 'y'){
+  alert('YES! I love Indian food!');
+} else if (userOne.toLowerCase() === 'no' || userOne.toLowerCase() === 'n') {
+  alert('Wrong! I love Indian food!');
+} else {
+  alert('You\'re silly, type yes or no');
+}
+
+var userMovie = prompt('Is my favorite TV show Stranger Things?');
 console.log('movie' + ":" + userMovie);
-if (userMovie.toLowerCase () === 'stranger things'){
-  alert('Wow! You\'re totally right, I love Stranger Things!');
+if (userMovie.toLowerCase () === 'yes' || userMovie.toLowerCase() === 'y'){
+  alert('Yes, I love Stranger Things');
+} else if (userMovie.toLowerCase() === 'no' || userMovie.toLowerCase === 'n'){
+  alert('What? I love the television show!');
 } else {
-  alert('C\'mon, no way, Stranger Things on Netflix is the best show!');
+  alert('You\'re silly, type yes or no');
 }
 
-var userCar = prompt('Guess the make of truck I drive!');
-console.log('car' + ":" + userCar);
-if (userCar.toLowerCase () === 'nissan'){
-  alert('Wow, you\'re right! I drive a Nissan Titan');
+var readMind = prompt('Can I read minds?');
+console.log('mind' + ":" + readMind);
+if (readMind.toLowerCase () === 'yes' || readMind.toLowerCase() === 'y'){
+  alert('ROFLOL! No, I cannot');
+} else if (readMind.toLowerCase() === 'no' || readMind.toLowerCase() === 'n') {
+  alert('You are correct, I can\'t read minds');
 } else {
-  alert('No way! I don\'t drive a ' + userCar + '!');
+  alert('You\'re silly, type yes or no');
 }
 
-var userGun = prompt('I am a firearms enthusiast, guess what my favorite gun brand is!');
-console.log('gun' + ":" + userGun);
-if (userGun.toLowerCase () === 'glock'){
-  alert('Wow, you\'re right! I love Glocks! They are the Toyota Camry of the gun world, ugly and reliable.');
+var longWalks = prompt('Do I like long walks on the beach?');
+console.log('walks' + ":" + longWalks);
+if (longWalks.toLowerCase () === 'yes' || longWalks.toLowerCase() === 'y'){
+  alert('ROFLOL! No, I like to drive though!');
+} else if (longWalks.toLowerCase() === 'no' || longWalks.toLowerCase() === 'n') {
+  alert('You are correct, I like to drive instead!');
 } else {
-  alert('No way! I don\'t like the ' + userGun + '!');
-}
-
-var userComputer = prompt('Guess what computer brand I like!');
-console.log('computer' + ":" + userComputer);
-if (userComputer.toLowerCase () === 'apple'){
-  alert('Wow, you\'re right! I love the Apple Macbooks!');
-} else {
-  alert('No way! I don\'t like ' + userComputer + '!');
-}
-
-var name= prompt("Okay, now tell me about you. What's your name?");
-
-function greeting () {
-  alert("Good to meet you " + name + "!");
-  console.log('name:' + name);
-}
-greeting();
-
-function eat () {
-  var food= prompt("Second, what do you like to eat?");
-  alert("Interesting! I enjoy eating " + food + " too! Last time I ate that, I was in " + food + "ville!");
-  console.log('food:' + food);
-
-}
-eat();
-
-function restaurant () {
-  var place= prompt("What restaurant do you like to eat at?");
-  alert("Yuck, I hate eating at " + place + "!");
-  console.log('place:' + place);
-}
-
-restaurant();
-
-function car () {
-  var drive= prompt("What kind of car do you drive?");
-  alert("I drive a Nissan, but I like the " + drive + " too!");
-  console.log('drive:' + drive);
-
-}
-car();
+  alert('You\'re silly, type yes or no');
+};
