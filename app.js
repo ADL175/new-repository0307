90480df96a8abb47d524892ce4da07c2cd9cf510
Adan.Rabadan <evilb4k4@gmail.com>
2017-03-09 13:43:02 -0800

    adding functions

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-"use strict";
+'use strict';
 var userScore = 0;
 
-var name= prompt("Hello, what's your name?");
+var name = prompt('Hello, what\'s your name?');
 
 function greeting () {
-  alert("Good to meet you " + name + "!");
+  alert('Good to meet you ' + name + '!');
   console.log('name:' + name);
 }
 greeting();
 
-confirm ("Let's play a guessing game, I have 7 questions for you, let's see if you can get a score of 7 out of 7!");
+confirm ('Let\'s play a guessing game, I have 7 questions for you, let\'s see if you can get a score of 7 out of 7!');
+
+function yesNoQuestions(){
 
 var question = ['Am I a vegetarian?','Do I like Indian food?', 'Is my favorite TV show Stranger Things?', 'Can I read minds?', 'Do I like long walks on the beach?'];
 
@@ -18,7 +20,6 @@ var answerYes = ['ROFLOL! Have you seen me?! No way. Omnivore.', 'YES! I love In
 var answerNo = ['You are correct, I eat everything!', 'Wrong! I love Indian food!', 'What? I love the television show!', 'You are correct, I can\'t read minds', 'You are correct, I like to drive instead!'];
 
 var varInc = [[0,1], [1,0], [1,0], [0,1], [0,1]];
-
 
 
 for (var i = 0; i < question.length; i++) {
@@ -34,51 +35,56 @@ for (var i = 0; i < question.length; i++) {
   }
   console.log('Score is: ' + userScore);
 }
+}
+yesNoQuestions();
 
-var tries = 0;
-while (tries < 4) {
-  var numberAnswer = prompt ('How many kids do I have?');
-  console.log('User name guess' + ":" + numberAnswer);
-  if (numberAnswer === '2' || numberAnswer.toLowerCase === 'two'){
-    alert('You\'re right! I have one four-year-old daughter and one-year-old daughter!');
-    userScore++;
-    console.log('User Score:' + userScore);
-    break;
-  } else if (numberAnswer > 2){
-    alert('No, that\'s too many!!! Try again!')
-    tries ++;
-  } else if (numberAnswer <2){
-    alert('Nope, I have more kids than that! Try again!');
-     tries++;
+function numQuestion(){
+  var tries = 0;
+  while (tries < 4) {
+    var numberAnswer = prompt ('How many kids do I have?');
+    console.log('User name guess' + ":" + numberAnswer);
+    if (numberAnswer === '2' || numberAnswer.toLowerCase === 'two'){
+      alert('You\'re right! I have one four-year-old daughter and one-year-old daughter!');
+      userScore++;
+      console.log('User Score:' + userScore);
+      break;
+    } else if (numberAnswer > 2){
+      alert('No, that\'s too many!!! Try again!')
+      tries ++;
+    } else if (numberAnswer <2){
+      alert('Nope, I have more kids than that! Try again!');
+       tries++;
+    }
   }
 }
-
-var kidNameArray = ['avery', 'zoe'];
-var tries = 0;
-while (tries < 6) {
-  var nameGuess = prompt('Can you guess one of the names of my daughters? One starts with an \'A\' and one with a \'Z\'.');
-  console.log('User name guess' + ":" + nameGuess);
-  if (nameGuess.toLowerCase () === kidNameArray[0]){
-    alert('Yes, my oldest 4-year-old is Avery! The other child is one-year-old Zoe!');
-    userScore++;
-    console.log('User Score:' + userScore);
-    break;
-  } else if (nameGuess.toLowerCase() === kidNameArray[1]) {
-    alert('Yes, my youngest daughter is Zoe! The other child is 4-year-old Avery!');
-    userScore++;
-    console.log('User Score:' + userScore);
-    break;
-  } else if (nameGuess.toLowerCase() !== kidNameArray[0] || nameGuess.toLowerCase() !== kidNameArray[1]){
-    alert('You\'re not even close!');
-    console.log('User Score:' + userScore);
-     tries++;
-  }
-};
-
-if (userScore === 7){
-  alert('Great job! Perfect Score, 7 out of 7 correct!');
-} else if (userScore <= 6){
-  alert('Not bad, but you missed a couple! Your score is ' + userScore + ' out of 7');
-} else if (userScore <= 4){
-  alert('Yikes, you don\'t know me very well! Your score is: ' + userScore + ' out of 7');
-}
+numQuestion();
+//
+// var kidNameArray = ['avery', 'zoe'];
+// var tries = 0;
+// while (tries < 6) {
+//   var nameGuess = prompt('Can you guess one of the names of my daughters? One starts with an \'A\' and one with a \'Z\'.');
+//   console.log('User name guess' + ":" + nameGuess);
+//   if (nameGuess.toLowerCase () === kidNameArray[0]){
+//     alert('Yes, my oldest 4-year-old is Avery! The other child is one-year-old Zoe!');
+//     userScore++;
+//     console.log('User Score:' + userScore);
+//     break;
+//   } else if (nameGuess.toLowerCase() === kidNameArray[1]) {
+//     alert('Yes, my youngest daughter is Zoe! The other child is 4-year-old Avery!');
+//     userScore++;
+//     console.log('User Score:' + userScore);
+//     break;
+//   } else if (nameGuess.toLowerCase() !== kidNameArray[0] || nameGuess.toLowerCase() !== kidNameArray[1]){
+//     alert('You\'re not even close!');
+//     console.log('User Score:' + userScore);
+//      tries++;
+//   }
+// };
+//
+// if (userScore === 7){
+//   alert('Great job! Perfect Score, 7 out of 7 correct!');
+// } else if (userScore <= 6){
+//   alert('Not bad, but you missed a couple! Your score is ' + userScore + ' out of 7');
+// } else if (userScore <= 4){
+//   alert('Yikes, you don\'t know me very well! Your score is: ' + userScore + ' out of 7');
+// }
